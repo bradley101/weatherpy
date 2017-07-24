9c28b18278b4c761cff6850f4196507cd081cee6
Shantanu <shantanu.banerjee.vt@gmail.com>
2017-07-24 12:52:40 +0530

    compute cmpl

--- setup.py
+++ setup.py
@@ -4,21 +4,13 @@ from setuptools import setup
 setup(
 
 	name = "wepy",
-	version='1.0',
+	version='1.1.0',
 	description='weather script for python - wepy',
 	url='https://github.com/bradley101/wepy',
 	author='Shantanu Banerjee',
 	author_email="shantanu.banerjee.vt@gmail.com",
 	license='MIT',
 	packages=['wepy'],
-		install_requires=[
-		'urllib',
-		'urllib2',
-		'json',
-		'os',
-		'sys',
-		'datetime'		
-	],
 	entry_points={
 		"console_scripts": [
 			"wepy=wepy.wepy:main"
